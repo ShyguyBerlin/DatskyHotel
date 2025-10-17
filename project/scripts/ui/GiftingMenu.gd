6f7f078afcdaef45c31df413f6801031f777de30
ShyguyBerlin <caspar.m.klein@gmail.com>
2025-10-17 17:10:53 +0200

    Gifting Menu stuff

---
+++ project/scripts/ui/GiftingMenu.gd
@@ -0,0 +1,11 @@
+extends Control
+
+var selected_item : int = -1
+
+func item_selected_final(index: int) -> void:
+	print("final selected",index)
+
+func item_selected(index: int) -> void:
+	print("selected",index)
+
+func 
