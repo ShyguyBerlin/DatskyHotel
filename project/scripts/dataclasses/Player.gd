007deddd188a550a5d8f0f387f90688672299172
ShyguyBerlin <caspar.m.klein@gmail.com>
2025-11-14 16:45:42 +0100

    add reward functions to action class. Action class is now meant for Player-input related actions. also add git addon to project, it might come in handy.

--- project/scripts/dataclasses/Player.gd
+++ project/scripts/dataclasses/Player.gd
@@ -1,5 +1,11 @@
 extends Resource
 class_name Player
 
-@export var money : float
+@export var money : float : set=set_money
 @export var inventory : Dictionary[StringName,int]
+
+signal money_changed
+
+func set_money(new_money):
+	money=new_money
+	money_changed.emit()
--- project/scripts/dataclasses/action/GiftAction.gd
+++ project/scripts/dataclasses/action/GiftAction.gd
@@ -2,7 +2,6 @@ extends Action
 class_name GiftAction
 
 var item_name
-var display_node # To play a "thank you" dialog
 
 func _init(item : String):
 	self.item_name=item
