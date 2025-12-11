cc9d8b682fee53eafe979fe926f9ccdd4f0cb195
Caspar Klein <58047693+ShyguyBerlin@users.noreply.github.com>
2025-12-11 02:19:32 +0100

    change FoodNeed to use TimeManager instead of process callback

--- project/scripts/dataclasses/habitant/needs/FoodNeed.gd
+++ project/scripts/dataclasses/habitant/needs/FoodNeed.gd
@@ -1,12 +1,13 @@
 extends HabitantNeed
 
-var foodLevel=30
+var foodLevel=4
 
 func unbind_habitant() -> void:
 	habitant.recieved_gift.disconnect(consume_gift_action)
 
 func bind_habitant() -> void:
 	habitant.recieved_gift.connect(consume_gift_action)
+	GameTimeManager.MediumCycleTick.connect(_on_food_cycle_tick)
 
 func consume_gift_action(action:GiftAction):
 	var item = action.get_item()
@@ -14,11 +15,22 @@ func consume_gift_action(action:GiftAction):
 		foodLevel+=item.nutritional_value
 
 func process(delta : float):
-	foodLevel-=delta
-	foodLevel=max(foodLevel,0)
+	pass
+
+func _on_food_cycle_tick(ticks):
+	# Expects a cycle to go about 20min
+	reduce_food_level(ticks * 60 * 20)
+	print("Food Cycle!")
+
+func reduce_food_level(seconds : float):
+	# Here we can influence how long 1 nutrient lasts
+	# 1-100 nutrients per food, maybe more
+	# 
+	# For now, one nutrient is worth five minutes of saturation
+	foodLevel-= seconds / 300
 
 func generate_request(residence : Residence) -> Array[Request]:
-	if foodLevel<=10:
+	if foodLevel<=6:
 		var req = FoodRequest.new(residence,habitant,self)
 		return [req]
 	return []
